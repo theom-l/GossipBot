24ad35fd31e70f5894514e7236468f016084e1c0
theom-l <85464878+theom-l@users.noreply.github.com>
2022-03-24 11:24:48 -0400

    Update gossip.py
    add temboo

--- gossip.py
+++ gossip.py
@@ -3,6 +3,11 @@ import sys
 import requests
 import json
 import openai
+
+from temboo.Library.Google.Gmail import SendEmail
+from temboo.core.session import TembooSession
+
+
 #https://jman4190.medium.com/how-to-build-a-gpt-3-chatbot-with-python-7b83e55805e6
 #https://python.plainenglish.io/create-ai-content-generator-with-python-flask-and-openai-gpt-3-407a19f096b
 #https://slacker.ro/2020/08/28/the-ultimate-guide-to-openais-gpt-3-language-model/
@@ -34,4 +39,28 @@ except requests.RequestException:
 gossip = answer['choices'][0]['text']
 
 print(gossip)
+
 #print(answer)
+
+def sendEmail(gossip):
+  # Create a session with your Temboo account details
+  session = TembooSession("theom-l", "myFirstApp", "LDlj0THtgAYNMjHJxhgpCWsI49Vfwuli")
+
+  # Instantiate the Choreo
+  sendEmailChoreo = SendEmail(session)
+
+  # Get an InputSet object for the Choreo
+  sendEmailInputs = sendEmailChoreo.new_input_set()
+
+  # Set the Choreo inputs
+  sendEmailInputs.set_Username("gb123988124123@gmail.com")
+  sendEmailInputs.set_Subject("1")
+  sendEmailInputs.set_ToAddress("emandinl@andrew.cmu.edu")
+  sendEmailInputs.set_Password("tpylvnfhlmuscdzo")
+  sendEmailInputs.set_MessageBody("1")
+
+  # Execute the Choreo
+  sendEmailResults = sendEmailChoreo.execute_with_results(sendEmailInputs)
+
+  # Print the Choreo outputs
+  print("Success: " + sendEmailResults.get_Success())
